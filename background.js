8c2ad24d96e8ddd64e79bed184f8f1fce4cca6e7
hwlee0103 <sdiverHyeWon0103@gmail.com>
2021-03-10 17:35:42 +0900

    setting background images
    set background of my html page

---
+++ background.js
@@ -0,0 +1,30 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 5;
+
+/*
+//required if we were using api
+function handleImgLoad() {
+  console.log("image loading");
+}
+*/
+function paintImage(imgNumber) {
+  const image = new Image();
+  image.src = `images/${imgNumber + 1}.jpg`;
+  image.classList.add("bgImage");
+  body.appendChild(image);
+  //required if we were using api
+  //image.addEventListener("loaded", handleImgLoad);
+}
+
+function genRandom() {
+  const number = Math.floor(Math.random() * IMG_NUMBER);
+  return number;
+}
+function init() {
+  //generate number
+  const randomNumber = genRandom();
+  paintImage(randomNumber);
+}
+
+init();
